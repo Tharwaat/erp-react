b189f35eeef794486ba237dc599d2b04e211a028
Tharwat <a.tharwat100@gmail.com>
2018-05-01 17:30:48 +0200

    Routing configuration, fixing navbar and header issue

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import './App.css';
-
+import { Switch, Route } from 'react-router-dom'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
-      
-      
         <Navbar img="/res/Logo.png" dots="/res/Dots.png"/>
-
+        
+        <Switch>
+          <Route exact path='/' render={() => <div>Home</div>}/>
+          <Route exact path='/biography' render={() => <div>Biography</div>}/>
+        </Switch>
+        
         <Footer/>
        
       </div>
