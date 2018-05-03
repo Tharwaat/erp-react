a8777231b03deddcce8f23d20be622a25781dd9b
Tharwat <a.tharwat100@gmail.com>
2018-05-03 12:42:31 +0200

    Media training component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import ContactUS from './components/ContactUs'
 import events from './components/Events'
+import MediaTraining from './components/MediaTraining'
+import md1 from './components/md1'
+import md2 from './components/md2'
 
 import './App.css';
 
@@ -11,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous"/>
         <Navbar img="/res/Logo.png" dots="/res/Dots.png"/>
         
         <Switch>
@@ -19,6 +22,9 @@ class App extends Component {
           <Route exact path='/biography' render={() => <div>Biography</div>}/>
           <Route exact path='/contact' component={ContactUS}/>
           <Route exact path='/events' component={events}/>
+          <Route exact path='/media' component={MediaTraining}/>
+          <Route exact path='/md1' component={md1}/>
+          <Route exact path='/md2' component={md2}/>
         </Switch>
         
         <Footer/>
---
+++ src/components/Articles.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import '../css/Articles.css';
+
+export default class Articles extends Component{
+    render(){
+        return(
+         <div>
+             
+         </div>
+        );
+    }
+}
